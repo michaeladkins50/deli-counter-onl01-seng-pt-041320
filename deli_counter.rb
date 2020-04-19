8cfc1978991ee93755a47fa24cd3639ee98d3a29
Michael Adkins <mikeadkins1112@gmail.com>
2020-04-19 13:15:44 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -16,7 +16,7 @@ def take_a_number(katz_deli, name)
   end
 end
 def now_serving(katz_deli)
-  if katz_deli == 0
+  if katz_deli.size == 0
     puts "There is nobody waiting to be served!"
   else
     puts "Currently serving #{other_deli.first}."
